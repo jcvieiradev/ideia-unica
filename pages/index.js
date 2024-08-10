23b094ffd34d26f573de43860de907ba1b302543
Julio Cesar Vieira <121468467+jcvieiradev@users.noreply.github.com>
2024-08-10 20:21:07 +0000

    pagina inicial

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,18 @@
-import Link from 'next/link';
-
+import Link from "next/link";
 
 function Home() {
-    return (
+  return (
     <div>
-        <div>
-        <h1>Home</h1>
-        <Link href="/sobre">
-        Acessar página Sobre
-        </Link>
-        </div>
+      <div>
+        <h1>Página inicial</h1>
+        <Link href="/sobre">Acessar página Sobre</Link>
+      </div>
 
-        <div>
-        <Link href="/tempo">
-        Acessar página Tempo
-        </Link>
-        </div>
+      <div>
+        <Link href="/tempo">Acessar página Tempo</Link>
+      </div>
     </div>
-    )
+  );
 }
 
-export default Home
+export default Home;
